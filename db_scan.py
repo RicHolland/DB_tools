978a3a8173ea9e563f9ff6c229c2252b16844c74
Ric Holland <ric.holland89@gmail.com>
2017-01-12 14:08:27 +0000

    Moved argument processing to the library file and removed commented debugging lines.
    Argument processing using argparse is now defined in the library file and called as a function.

--- db_scan.py
+++ db_scan.py
@@ -1,4 +1,37 @@
-import pyodbc, getpass
+import pyodbc, getpass, argparse
+
+def getParameters():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--driver', dest='driver')
+    parser.add_argument('-s', '--server', dest='server')
+    parser.add_argument('-p', '--port', dest='port')
+    parser.add_argument('-b', '--database', dest='database')
+    parser.add_argument('-u', '--user', dest='user')
+
+    args = vars(parser.parse_args())
+
+    argsList = ['driver', 'server', 'port', 'database', 'user']
+
+    n=0
+
+    print('Enter missing connection data:')
+    for x in argsList:
+        if args[x]:
+            continue
+        else:
+            args[x] = raw_input(x + ':\t')
+            n=n+1
+
+    if (n == 0):
+        print('All connection data entered.')
+
+    print('Review connection data:')
+    for y in argsList:
+        print('\t{0}:\t{1}'.format(y, args[y]))
+
+    return args
+
 
 def connect(dic):
     cnxnStr = "DRIVER=" + dic['driver'] + ";SERVER=" + dic['server'] + ";PORT=" + dic['port'] + ";DATABASE=" + dic['database'] + ";UID=" + dic['user'] + ";PWD=" + getpass.getpass()
@@ -18,15 +51,7 @@ def linecount(cnxn):
             continue
         cursor.execute("SELECT count(*) FROM {0}.{1}.{2}".format(r[0], r[1], r[2]))
         x = cursor.fetchone()
-        #print(type(x))
-        #print(type(x[0]))
-        #print(type(r))
-        #print(type(r[0]))
         print(r[0] + "," + r[1] + "," + r[2] + "," + str(x[0]))
-        #break
 
 def promptForParameter(prompt):
     return raw_input(prompt)
-
-#c = connect("FreeTDS", "haldexcommerce.cloudapp.net", "57500", "HaldexCommerce", "gabor")
-#linecount(c)
